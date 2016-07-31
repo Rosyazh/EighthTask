a1a82225d77c2230ee253218239209f94d12c9df
Rosyazh <rossol.zheka@gmail.com>
2016-08-01 00:28:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,47 +11,51 @@ Make possible to sort a list of Employees by salary or by personal info.
 
 namespace EighthTask
 {
-    class Employee
+    class Employee: IComparable<Employee>
     {
         public string Name { get; set; }
         public string Surname { get; set; }
         public double Salary { get; set; }
+
+        public int CompareTo(Employee other)
+        {
+            if (Equals(other)) return 0;
+            return Surname.CompareTo(other.Surname);
+        }
+    }
+
+    class EmployeeComparer : IComparer<Employee>
+    {
+        public int Compare(Employee _rab1, Employee _rab2)
+        {
+            if (_rab1.Salary > _rab2.Salary)
+                return -1;
+            else if (_rab1.Salary < _rab2.Salary)
+                return 1;
+            else
+                return 0;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
-        {
-            Employee r1 = new Employee { Name = "Arsenij", Surname = "Nikiforov", Salary = 570.52 };
-            Employee r2 = new Employee { Name = "Vitalij", Surname = "Abramov", Salary = 450.52 };
-            Employee r3 = new Employee { Name = "Boris", Surname = "Zurupov", Salary = 320.12 };
+        { 
+            var employees = new List<Employee>();
 
-            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { Name = "Boris", Surname = "Nikiforov", Salary = 570.52 });
+            employees.Add(new Employee { Name = "Vitalij", Surname = "Abramov", Salary = 250.52 });
+            employees.Add(new Employee { Name = "Alina", Surname = "Zuratova", Salary = 320.12 });
+            employees.Add(new Employee { Name = "Olya", Surname = "Rusaya", Salary = 450.89 });
+            employees.Add(new Employee { Name = "Adam", Surname = "Zako", Salary = 320.12 });
 
-            employees.Add(r1);
-            employees.Add(r2);
-            employees.Add(r3);
+            employees.Sort(); //sorting by name
+            employees.Sort(new EmployeeComparer()); //descending sorting by salary
 
-            employees.Sort(delegate (Employee rab1, Employee rab2)
-            { return rab1.Salary.CompareTo(rab2.Salary); });
-
-            Console.WriteLine("Sorting by salary.");
-            foreach (Employee em in employees)
-                Console.WriteLine(em.Salary + " " + em.Name + " " + em.Surname);
-
-            employees.Sort(delegate (Employee rab1, Employee rab2)
-            { return rab1.Name.CompareTo(rab2.Name); });
-
-            Console.WriteLine("\nSorting by name.");
-            foreach (Employee em in employees)
-                Console.WriteLine(em.Salary.ToString() + " " + em.Name + " " + em.Surname);
-
-            employees.Sort(delegate (Employee rab1, Employee rab2)
-            { return rab1.Surname.CompareTo(rab2.Surname); });
-
-            Console.WriteLine("\nSorting by surname.");
-            foreach (Employee em in employees)
-                Console.WriteLine(em.Salary.ToString() + " " + em.Name + " " + em.Surname);
+            foreach (Employee rab in employees)
+            {
+                Console.WriteLine("{0} {1} {2}", rab.Name.PadRight(15), rab.Surname.PadRight(15), rab.Salary.ToString().PadRight(10));
+            }
 
             Console.ReadKey();
         }
